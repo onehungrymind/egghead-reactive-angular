4603e362d4cb327967059b36fbccdf385b11b7eb
Lukas Ruebbelke <simpul@gmail.com>
2018-11-11 06:50:47 -0700

    update: revamping visuals

--- apps/dashboard/src/app/projects/projects.module.ts
+++ apps/dashboard/src/app/projects/projects.module.ts
@@ -1,27 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsComponent } from './projects.component';
 import { ProjectsListComponent } from './projects-list/projects-list.component';
 import { ProjectDetailsComponent } from './project-details/project-details.component';
 import { MaterialModule } from '@workshop/material';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { CoreDataModule } from '@workshop/core-data';
 
 @NgModule({
   imports: [
     CommonModule,
-    ProjectsRoutingModule,
     MaterialModule,
-    ReactiveFormsModule,
     FormsModule,
-    CoreDataModule
+    CoreDataModule,
   ],
   declarations: [
     ProjectsComponent,
     ProjectsListComponent,
     ProjectDetailsComponent,
+  ],
+  exports: [
+    ProjectsComponent,
   ]
 })
 export class ProjectsModule { }
