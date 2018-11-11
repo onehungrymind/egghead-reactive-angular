2eb42836d32a3db90c1efb69c1b0598a2f87983d
Lukas Ruebbelke <simpul@gmail.com>
2018-11-11 06:50:47 -0700

    update: revamping visuals

--- apps/dashboard/src/app/app-routing.module.ts
+++ apps/dashboard/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '@workshop/ui-login';
+import { ProjectsComponent } from './projects/projects.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'projects', pathMatch: 'full'},
-  {path: 'projects', loadChildren: './projects/projects.module#ProjectsModule'},
+  {path: 'projects', component: ProjectsComponent },
   {path: 'login', component: LoginComponent},
   {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
