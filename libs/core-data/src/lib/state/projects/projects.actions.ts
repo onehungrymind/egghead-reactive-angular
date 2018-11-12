14b0592d7c7251298cd401b22890b5b60683cb98
Lukas Ruebbelke <simpul@gmail.com>
2018-11-11 18:10:54 -0700

    update: setting up effects

--- libs/core-data/src/lib/state/projects/projects.actions.ts
+++ libs/core-data/src/lib/state/projects/projects.actions.ts
@@ -6,40 +6,48 @@ export enum ProjectsActionTypes {
   ProjectSelected = '[Projects] Selected',
   LoadProjects = '[Projects] Load Data',
   ProjectsLoaded = '[Projects] Data Loaded',
-
   AddProject = '[Projects] Add Data',
   ProjectAdded = '[Projects] Data Added',
-
   UpdateProject = '[Projects] Update Data',
+  ProjectUpdated = '[Projects] Data Updated',
   DeleteProject = '[Projects] Delete Data',
+  ProjectDeleted = '[Projects] Date Deleted',
 }
 
 export class SelectProject implements Action {
   readonly type = ProjectsActionTypes.ProjectSelected;
-  constructor(public payload: Project) {}
+  constructor(public payload: Project) { }
 }
 export class LoadProjects implements Action {
   readonly type = ProjectsActionTypes.LoadProjects;
 }
 export class ProjectsLoaded implements Action {
   readonly type = ProjectsActionTypes.ProjectsLoaded;
-  constructor(public payload: Project[]) {}
+  constructor(public payload: Project[]) { }
 }
 export class AddProject implements Action {
   readonly type = ProjectsActionTypes.AddProject;
-  constructor(public payload: Project) {}
+  constructor(public payload: Project) { }
 }
 export class ProjectAdded implements Action {
   readonly type = ProjectsActionTypes.ProjectAdded;
-  constructor(public payload: Project) {}
+  constructor(public payload: Project) { }
 }
 export class UpdateProject implements Action {
   readonly type = ProjectsActionTypes.UpdateProject;
-  constructor(public payload: Project) {}
+  constructor(public payload: Project) { }
+}
+export class ProjectUpdated implements Action {
+  readonly type = ProjectsActionTypes.ProjectUpdated;
+  constructor(public payload: Project) { }
 }
 export class DeleteProject implements Action {
   readonly type = ProjectsActionTypes.DeleteProject;
-  constructor(public payload: Project) {}
+  constructor(public payload: Project) { }
+}
+export class ProjectDeleted implements Action {
+  readonly type = ProjectsActionTypes.ProjectDeleted;
+  constructor(public payload: Project) { }
 }
 
 export type ProjectsActions = SelectProject
@@ -48,5 +56,7 @@ export type ProjectsActions = SelectProject
   | AddProject
   | ProjectAdded
   | UpdateProject
+  | ProjectUpdated
   | DeleteProject
+  | ProjectDeleted
   ;
--- libs/core-data/src/lib/state/projects/projects.effects.ts
+++ libs/core-data/src/lib/state/projects/projects.effects.ts
@@ -1,36 +1,47 @@
 import { Injectable } from '@angular/core';
-import { Actions, Effect } from '@ngrx/effects';
-import { DataPersistence } from '@nrwl/nx';
-import { map } from 'rxjs/operators';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { map, switchMap } from 'rxjs/operators';
 
 import { Project } from './../../projects/project.model';
 import { ProjectsService } from './../../projects/projects.service';
-import { ProjectsActionTypes, ProjectsLoaded, ProjectAdded, LoadProjects, AddProject } from './projects.actions';
-import { ProjectsState } from './projects.reducer';
+import { AddProject, LoadProjects, ProjectAdded, ProjectsActionTypes, ProjectsLoaded, UpdateProject, ProjectUpdated, DeleteProject, ProjectDeleted } from './projects.actions';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class ProjectsEffects {
-  @Effect() loadProjects$
-    = this.dataPersistence.fetch(ProjectsActionTypes.LoadProjects, {
-    run: (action: LoadProjects, state: ProjectsState) => {
-      return this.projectsService.all()
+  @Effect() loadProjects$ = this.actions$.pipe(
+    ofType(ProjectsActionTypes.LoadProjects),
+    switchMap((action: LoadProjects) =>
+      this.projectsService.all()
         .pipe(map((res: Project[]) => new ProjectsLoaded(res)))
-    },
-    onError: () => {}
-  })
+    )
+  );
 
-  @Effect() addProjects$
-    = this.dataPersistence.pessimisticUpdate(ProjectsActionTypes.AddProject, {
-      run: (action: AddProject, state: ProjectsState) => {
-        return this.projectsService.create(action.payload)
-          .pipe(map((res: Project) => new ProjectAdded(res)))
-      },
-    onError: () => {}
-  })
+  @Effect() addProject$ = this.actions$.pipe(
+    ofType(ProjectsActionTypes.AddProject),
+    switchMap((action: AddProject) =>
+      this.projectsService.create(action.payload)
+        .pipe(map((res: Project) => new ProjectAdded(res)))
+    )
+  );
+
+  @Effect() updateProject$ = this.actions$.pipe(
+    ofType(ProjectsActionTypes.UpdateProject),
+    switchMap((action: UpdateProject) =>
+      this.projectsService.update(action.payload)
+        .pipe(map((res: Project) => new ProjectUpdated(res)))
+    )
+  );
+
+  @Effect() deleteProject$ = this.actions$.pipe(
+    ofType(ProjectsActionTypes.DeleteProject),
+    switchMap((action: DeleteProject) =>
+      this.projectsService.create(action.payload)
+        .pipe(map((res: Project) => new ProjectDeleted(res)))
+    )
+  );
 
   constructor(
     private actions$: Actions,
-    private dataPersistence: DataPersistence<ProjectsState>,
     private projectsService: ProjectsService
-  ) {}
+  ) { }
 }
